e3ad52b04044e74c92939ddd02d64f94b387cef9
DevelopItNow <kvnjager@gmail.com>
2019-03-20 11:59:24 +0100

    Dashboard header in include file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,20 +13,7 @@
 								{{ session('status') }}
 							</div>
 						@endif
-						<div class="links text-center">
-							<a href="{{route('account.create')}}">{{__('dashboard.account')}}</a>
-							<a href="">{{__('dashboard.request')}}</a>
-							<a href="">{{__('dashboard.group')}}</a>
-							<a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-							   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								{{__('dashboard.overview')}}
-							</a>
-							<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-								<a class="dropdown-item" href="#">{{__('dashboard.accounts')}}</a>
-								<a class="dropdown-item" href="#">{{__('dashboard.requests')}}</a>
-								<a class="dropdown-item" href="#">{{__('dashboard.groups')}}</a>
-							</div>
-						</div>
+						@include('inc.dashboard_header')
 					</div>
 				</div>
 			</div>
---
+++ resources/views/inc/dashboard_header.blade.php
@@ -0,0 +1,14 @@
+<div class="links text-center">
+	<a href="{{route('account.create')}}">{{__('dashboard.account')}}</a>
+	<a href="">{{__('dashboard.request')}}</a>
+	<a href="">{{__('dashboard.group')}}</a>
+	<a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+	   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		{{__('dashboard.overview')}}
+	</a>
+	<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+		<a class="dropdown-item" href="#">{{__('dashboard.accounts')}}</a>
+		<a class="dropdown-item" href="#">{{__('dashboard.requests')}}</a>
+		<a class="dropdown-item" href="#">{{__('dashboard.groups')}}</a>
+	</div>
+</div>
