b892c18d1299227da5926e526e1cc71434c5e68d
Cornayy <cornegamer@live.nl>
2019-03-23 21:10:35 +0100

    Added icons for languages

--- resources/views/inc/language_dropdown.blade.php
+++ resources/views/inc/language_dropdown.blade.php
@@ -3,7 +3,7 @@
         {{__('header.language')}} <span class="caret"></span>
     </a>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="/lang/nl">{{__('header.dutch')}}</a>
-        <a class="dropdown-item" href= "/lang/en">{{__('header.english')}}</a>
+        <a class="dropdown-item" href="/lang/nl"><i><span class="mr-1" role="img" id="nl"></span></i>{{__('header.dutch')}}</a>
+        <a class="dropdown-item" href= "/lang/en"><i><span class="mr-1" role="img" id="en"></span></i>{{__('header.english')}}</a>
     </div>
 </div>
