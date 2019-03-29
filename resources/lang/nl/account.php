f9dc6e7e89f3e47eedc61d7890cf4a6dd4354108
Cornayy <cornegamer@Live.nl>
2019-03-29 12:49:10 +0100

    Changed account to rekening

--- resources/lang/nl/account.php
+++ resources/lang/nl/account.php
@@ -2,22 +2,22 @@
 
     return [
 
-        'create_account' => 'Voeg account toe',
+        'create_account' => 'Voeg rekening toe',
         'name' => 'Naam',
         'account_number' => 'Rekeningnummer',
         'created_at' => 'Aangemaakt op',
         'action' => 'Actie',
-        'delete_account' => 'Verwijder account',
+        'delete_account' => 'Verwijder rekening',
         'edit' => 'Inzien',
-        'edit_account' => 'Wijzig account',
+        'edit_account' => 'Wijzig rekening',
         'delete' => 'Verwijder',
-        'delete_confirm' => 'Weet je zeker dat je dit account wilt verwijderen?',
+        'delete_confirm' => 'Weet je zeker dat je deze rekening wilt verwijderen?',
         'income' => 'Inkomsten',
 
-        'account_added' => 'Account is toegevoegd',
-        'account_changed' => 'Account is gewijzigd',
-        'account_deleted' => 'Account is verwijderd',
-        'account_export' => 'Accounts exporteren',
+        'account_added' => 'Rekening is toegevoegd',
+        'account_changed' => 'Rekening is gewijzigd',
+        'account_deleted' => 'Rekening is verwijderd',
+        'account_export' => 'Rekeningen exporteren',
         'export_success' => 'Exporteren gelukt',
         'export_failed' => 'Exporteren mislukt, heb je een valide token gebruikt?',
 
