fa885373788df82261c2e5847c590aea5e86610d
Cornayy <cornegamer@Live.nl>
2019-03-29 13:42:59 +0100

    Planned Payments changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,7 +114,7 @@
             dayNamesShort: ['zon', 'maa', 'din', 'woe', 'don', 'vri', 'zat'],
             dayNamesMin: ['zo', 'ma', 'di', 'wo', 'do', 'vr', 'za'],
             weekHeader: 'Wk',
-            dateFormat: 'dd-mm-yy',
+            dateFormat: 'dd/mm/yy',
             firstDay: 1,
             isRTL: false,
             showMonthAfterYear: false,
